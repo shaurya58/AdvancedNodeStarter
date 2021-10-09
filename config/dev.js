705215e20451fe08e2931ad47a4696ab6edf68b5
Shaurya Sisodia <38223668+shaurya58@users.noreply.github.com>
2021-10-09 15:29:08 +0530

    Removed exposed secrets from dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,7 @@
 module.exports = {
-  googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI:
-    "mongodb+srv://shaurya58:shaurya58@cluster0.kaiwp.mongodb.net/blogster?retryWrites=true&w=majority",
+  googleClientID: "<google_client_id>",
+  googleClientSecret: "<google_client_secret>",
+  mongoURI: "<mongo_db_instance_uri>",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
 };
